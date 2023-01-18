cefd2677a02d3de238d4919215996e12a8618da8
Hadia Shafique <hshafique@Hadias-MacBook-Air.local>
2023-01-18 15:27:26 -0600

    first commit

---
+++ data.js
@@ -0,0 +1,12 @@
+const userData = [
+    {
+
+    name: "Hadia",
+    email: "hshafiquedev@gmail.com",
+    password: "Hello1234",
+    isAdmin: true,
+}
+    
+]
+
+module.exports = userData;
---
+++ server.js
@@ -0,0 +1,46 @@
+const cors = require('cors')
+const express = require('express')
+const app = express()
+require("dotenv").config()
+const mongoose = require("mongoose")
+const db = mongoose.connection
+const mongoURI = process.env.MONGO_URI
+const PORT = process.env.PORT || 3001
+const User = require('./models/User')
+const userData = require('./data')
+//connecting to my database
+//connecting to MongoDB
+mongoose.connect(mongoURI, { useNewUrlParser: true},
+    () => console.log('MongoDB connection establish') )
+ 
+
+// Error / Disconnection
+db.on('error', err => console.log(err.message + ' is Mongod not running?'))
+db.on('disconnected', () => console.log('mongo disconnected'))
+
+
+// Middleware
+app.use(express.urlencoded({ extended: false }))// extended: false - does not allow nested objects in query strings
+app.use(express.json()); //use .json(), not .urlencoded()
+app.use(express.static('public')) // we need to tell express to use the public directory for static files... this way our app will find index.html as the route of the application! We can then attach React to that file!
+app.use(cors())
+
+
+
+app.get('/users', async (req,res)=>{
+    User.find({}, (err, foundUsers)=>{
+        res.json(foundUsers)
+    })
+})
+
+// seeding the db
+app.get('/seed', async (req, res) => {
+    await User.deleteMany({});
+    await User.insertMany(userData);
+    res.send('done!');
+  })
+
+
+app.listen(PORT, ()=> {
+    console.log(`Listening on ${PORT} `)
+})
